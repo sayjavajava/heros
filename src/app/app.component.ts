90fccb82c6a70eb46b2de0f36ba99601d81d47f4
sayjavajava <waqasrana11@gmail.com>
2017-11-27 16:55:41 +0500

    first commit on nov

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <nav>
+      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
 }
---
+++ src/app/hero-search.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+
+import { Hero } from './hero';
+
+@Injectable()
+export class HeroSearchService {
+
+  constructor(private http: Http) {}
+
+  search(term: string): Observable<Hero[]> {
+    return this.http
+      .get(`http://localhost:8080/users/find/?name=${term}`)
+      .map((r: Response) => r.json()._embedded.heroes as Hero[]);
+    }
+}
